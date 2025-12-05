2ec1cbc81edfa2233a830f203513e8444b32d5eb
Jason Barto <jason.p.barto@gmail.com>
2025-12-05 12:55:32 +0000

    chanaged install method for sam cli

--- acme-cicd/build.sh
+++ acme-cicd/build.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
-# Assumes a recent version of Ubuntu
-# Update package lists and update dependencies
-export DEBIAN_FRONTEND=noninteractive
-apt-get update -y
-apt-get install -y \
-    build-essential \
-    python3-pip \
-    python3-venv \
-    unzip \
-    wget
+# Assumes a recent version of Ubuntu with python3, unzip, and wget pre-installed
 
-# Install the AWS SAM CLI v2
-python3 -m pip install aws-sam-cli
+# Download the AWS SAM CLI installer for Linux x86_64
+wget https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip
+
+# Unzip the installer
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+
+# Install AWS SAM CLI
+./sam-installation/install
+
+# Clean up installation files
+rm -rf aws-sam-cli-linux-x86_64.zip aws-sam-cli-linux-x86_64.zip.sha256 sam-installation
 
 # Verify the installation
 sam --version
